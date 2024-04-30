ab008774bf1c9d28675e65058f8176a3e985996e
Maurits van der Schee <maurits@vdschee.nl>
2024-04-30 23:41:19 +0200

    add proxy

---
+++ proxy/proxy.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/cookiejar"
+
+	"github.com/lxzan/gws"
+)
+
+func main() {
+	upgrader := gws.NewUpgrader(&Handler{}, &gws.ServerOption{
+		CheckUtf8Enabled: true,
+		Recovery:         gws.Recovery,
+		PermessageDeflate: gws.PermessageDeflate{
+			Enabled:               true,
+			ServerContextTakeover: true,
+			ClientContextTakeover: true,
+		},
+	})
+	http.HandleFunc("/connect", func(writer http.ResponseWriter, request *http.Request) {
+		socket, err := upgrader.Upgrade(writer, request)
+		if err != nil {
+			return
+		}
+		go func() {
+			socket.ReadLoop()
+		}()
+	})
+	log.Panic(
+		http.ListenAndServe(":4000", nil),
+	)
+}
+
+type Handler struct {
+	gws.BuiltinEventHandler
+	jar *cookiejar.Jar
+}
+
+func (c *Handler) OnPing(socket *gws.Conn, payload []byte) {
+	_ = socket.WritePong(payload)
+}
+
+func (c *Handler) OnMessage(socket *gws.Conn, message *gws.Message) {
+	defer message.Close()
+	if c.jar == nil {
+		c.jar, _ = cookiejar.New(nil)
+	}
+	client := &http.Client{
+		Jar: c.jar,
+	}
+	resp, err := client.Get("http://localhost:5000")
+	if err != nil {
+		_ = socket.WriteMessage(message.Opcode, []byte("connect failed"))
+	}
+	b := []byte{}
+	if err == nil {
+		b, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			_ = socket.WriteMessage(message.Opcode, []byte("read failed"))
+		}
+		resp.Body.Close()
+	}
+	//time.Sleep(1000 * time.Millisecond)
+	_ = socket.WriteMessage(message.Opcode, b)
+}
--- server/server.go
+++ server/server.go
@@ -1,68 +1,18 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/http/cookiejar"
 	"time"
 
-	"github.com/lxzan/gws"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	upgrader := gws.NewUpgrader(&Handler{}, &gws.ServerOption{
-		CheckUtf8Enabled: true,
-		Recovery:         gws.Recovery,
-		PermessageDeflate: gws.PermessageDeflate{
-			Enabled:               true,
-			ServerContextTakeover: true,
-			ClientContextTakeover: true,
-		},
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		time.Sleep(1000)
+		return c.SendString("Hello, Worlds!")
 	})
-	http.HandleFunc("/connect", func(writer http.ResponseWriter, request *http.Request) {
-		socket, err := upgrader.Upgrade(writer, request)
-		if err != nil {
-			return
-		}
-		go func() {
-			socket.ReadLoop()
-		}()
-	})
-	log.Panic(
-		http.ListenAndServe(":4000", nil),
-	)
-}
 
-type Handler struct {
-	gws.BuiltinEventHandler
-	jar *cookiejar.Jar
-}
-
-func (c *Handler) OnPing(socket *gws.Conn, payload []byte) {
-	_ = socket.WritePong(payload)
-}
-
-func (c *Handler) OnMessage(socket *gws.Conn, message *gws.Message) {
-	defer message.Close()
-	if c.jar == nil {
-		c.jar, _ = cookiejar.New(nil)
-	}
-	client := &http.Client{
-		Jar: c.jar,
-	}
-	resp, err := client.Get("http://localhost:4000")
-	if err != nil {
-		_ = socket.WriteMessage(message.Opcode, []byte("connect failed"))
-	}
-	b := []byte{}
-	if err == nil {
-		b, err = ioutil.ReadAll(resp.Body)
-		if err != nil {
-			_ = socket.WriteMessage(message.Opcode, []byte("read failed"))
-		}
-		resp.Body.Close()
-	}
-	time.Sleep(1000 * time.Millisecond)
-	_ = socket.WriteMessage(message.Opcode, b)
+	app.Listen(":5000")
 }

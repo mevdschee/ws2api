bbc218649215fa5517168e36e55bb69ec94fe2a7
Maurits van der Schee <maurits@vdschee.nl>
2024-06-22 01:19:56 +0200

    add in-order

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -46,22 +46,20 @@ func fetchDataWithRetries(c *http.Client, url string, body string) (r *http.Resp
 }
 
 var request_count = 0
-var request_count_channel chan int
-
 var curl_count = 0
-var curl_count_channel chan int
+var count_channel chan int
 
 func main() {
 	handler := Handler{
 		sessions: gws.NewConcurrentMap[string, *gws.Conn](16),
-		channels: gws.NewConcurrentMap[string, *chan bool](16),
 	}
 	serverOptions := gws.ServerOption{
 		CheckUtf8Enabled:  true,
 		Recovery:          gws.Recovery,
 		PermessageDeflate: gws.PermessageDeflate{Enabled: false},
-		ParallelEnabled:   true,
-		ParallelGolimit:   16,
+		// keep disabled to ensure packet order
+		//		ParallelEnabled:   true,
+		//		ParallelGolimit:   16,
 	}
 	upgrader := gws.NewUpgrader(&handler, &serverOptions)
 	http.HandleFunc("/connect", func(writer http.ResponseWriter, request *http.Request) {
@@ -72,11 +70,7 @@ func main() {
 		go func() {
 			handler.sessions.Store(request.RemoteAddr, socket)
 			socket.WriteString(request.RemoteAddr)
-			handling := make(chan bool, 1)
-			handling <- true
-			handler.channels.Store(request.RemoteAddr, &handling)
 			socket.ReadLoop()
-			handler.channels.Delete(request.RemoteAddr)
 			handler.sessions.Delete(request.RemoteAddr)
 		}()
 	})
@@ -90,8 +84,7 @@ func main() {
 		_ = socket.WriteString(string(b))
 	})
 	// log session and request counts (start)
-	request_count_channel = make(chan int, 10000)
-	curl_count_channel = make(chan int, 1000000)
+	count_channel = make(chan int, 1000000)
 	ticker := time.NewTicker(time.Second)
 	go func() {
 		for {
@@ -104,11 +97,11 @@ func main() {
 	go func() {
 		for {
 			select {
-			case c := <-request_count_channel:
-				request_count += c
-				break
-			case d := <-curl_count_channel:
-				curl_count += d
+			case c := <-count_channel:
+				if c > 0 {
+					request_count += c
+				}
+				curl_count += c
 				break
 			}
 		}
@@ -122,7 +115,6 @@ func main() {
 type Handler struct {
 	gws.BuiltinEventHandler
 	sessions *gws.ConcurrentMap[string, *gws.Conn]
-	channels *gws.ConcurrentMap[string, *chan bool]
 }
 
 func (c *Handler) OnPing(socket *gws.Conn, payload []byte) {
@@ -130,15 +122,11 @@ func (c *Handler) OnPing(socket *gws.Conn, payload []byte) {
 }
 
 func (c *Handler) OnMessage(socket *gws.Conn, message *gws.Message) {
-	// ensure in-order start
-	handling, _ := c.channels.Load(socket.RemoteAddr().String())
-	<-*handling
-	// ensure in-order end
 	defer message.Close()
 	client := &http.Client{}
-	curl_count_channel <- 1
+	count_channel <- 1
 	resp, err := fetchDataWithRetries(client, "http://localhost:5000?addr="+url.QueryEscape(socket.RemoteAddr().String()), message.Data.String())
-	curl_count_channel <- -1
+	count_channel <- -1
 	if err != nil {
 		_ = socket.WriteMessage(message.Opcode, []byte("connect failed"))
 	}
@@ -152,8 +140,5 @@ func (c *Handler) OnMessage(socket *gws.Conn, message *gws.Message) {
 	}
 	//time.Sleep(1000 * time.Millisecond)
 	_ = socket.WriteMessage(message.Opcode, b)
-	request_count_channel <- 1
-	// ensure in-order
-	*handling <- true
 	//_ = socket.WriteString(fmt.Sprintf("len: %v\n", c.sessions.Len()))
 }

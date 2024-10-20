a3017dc729a523920ffe8ef1503ca28f8271c9b9
Maurits van der Schee <maurits@vdschee.nl>
2024-10-20 08:13:31 +0200

    update

--- server/index.php
+++ server/index.php
@@ -7,15 +7,18 @@
 //
 // to test out of order
 //if (explode(':', $_GET['addr'])[1] % 4 == 0) usleep(random_int(0, 100) * 10000);
-echo json_encode(sprintf("I got '%s' via '%s' from '%s'", $HTTP_RAW_POST_DATA, $_SERVER['REMOTE_ADDR'] . ':4000', $_SERVER['PATH_INFO']));
-
-// test remote initiated message
-if (random_int(0, 100) == 0) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://" . $_SERVER['REMOTE_ADDR'] . ':4000/' . explode('/', $_SERVER['PATH_INFO'])[3]);
-    $payload = json_encode([3, "123", ["reply-from-php" => true]]);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_exec($ch);
-    curl_close($ch);
+if (explode('/', $_SERVER['PATH_INFO'])[1] == "connect") {
+    echo "\"ok\"";
+} else {
+    echo json_encode(sprintf("I got '%s' via '%s' from '%s'", $HTTP_RAW_POST_DATA, $_SERVER['REMOTE_ADDR'] . ':4000', $_SERVER['PATH_INFO']));
+    // test remote initiated message
+    if (random_int(0, 100) == 0) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://" . $_SERVER['REMOTE_ADDR'] . ':4000/' . explode('/', $_SERVER['PATH_INFO'])[3]);
+        $payload = json_encode([2, "123", ["call-from-php" => true]]);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_exec($ch);
+        curl_close($ch);
+    }
 }

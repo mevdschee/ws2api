1b4f3831ca256df789cd0906b1eefcdcd1d6e132
Maurits van der Schee <maurits@vdschee.nl>
2024-10-25 11:11:19 +0200

    parallel tests

--- wsproxy/wsproxy_test.go
+++ wsproxy/wsproxy_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -51,6 +53,78 @@ func TestCannotConnect(t *testing.T) {
 	}
 	got := err.Error()
 	want := "websocket: bad handshake"
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// TestIncomingMessage tries to connect with a websocket and sends
+// and receives a message in text format over that websocket connection
+func TestIncomingMessage(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			w.Write([]byte("ok"))
+			return
+		}
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			t.Errorf("error reading body: %q", err.Error())
+		}
+		got := r.Method + " " + r.RequestURI + " " + string(bodyBytes)
+		want := "POST /test request_message"
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+		w.Write([]byte("response_message"))
+	}))
+	defer server.Close()
+	wsServer := httptest.NewServer(getWsHandler(server.URL + "/"))
+	defer wsServer.Close()
+	wsUrl := strings.Replace(wsServer.URL, "http://", "ws://", 1)
+	wsClient, _, err := websocket.DefaultDialer.Dial(wsUrl+"/test", nil)
+	if wsClient != nil {
+		defer wsClient.Close()
+	}
+	if err != nil {
+		t.Errorf("error connecting ws client: %s", err.Error())
+	}
+	wsClient.WriteMessage(websocket.TextMessage, []byte("request_message"))
+	messageType, messageBytes, err := wsClient.ReadMessage()
+	if err != nil {
+		t.Errorf("error reading from ws client: %s", err.Error())
+	}
+	got := fmt.Sprintf("%d %s", messageType, string(messageBytes))
+	want := "1 response_message" // 1 = text message
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+// TestOutgoingMessage tries to connect with a websocket and sends
+// and receives a message in text format over that websocket connection
+func TestOutgoingMessage(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	}))
+	wsServer := httptest.NewServer(getWsHandler(server.URL + "/"))
+	defer wsServer.Close()
+	wsUrl := strings.Replace(wsServer.URL, "http://", "ws://", 1)
+	wsClient, _, err := websocket.DefaultDialer.Dial(wsUrl+"/test", nil)
+	if wsClient != nil {
+		defer wsClient.Close()
+	}
+	if err != nil {
+		t.Errorf("error connecting ws client: %s", err.Error())
+	}
+	// make post request
+	c := &http.Client{}
+	c.Post(wsServer.URL+"/test", "plain/text", strings.NewReader("server_message"))
+	messageType, messageBytes, err := wsClient.ReadMessage()
+	if err != nil {
+		t.Errorf("error reading from ws client: %s", err.Error())
+	}
+	got := fmt.Sprintf("%d %s", messageType, string(messageBytes))
+	want := "1 server_message" // 1 = text message
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
 	}

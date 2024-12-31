922e7413b676eba9f2c4a606517182214b8023d0
Maurits van der Schee <maurits@vdschee.nl>
2024-12-31 08:00:23 +0100

    Update optimize.sh

--- wsproxy/optimize.sh
+++ wsproxy/optimize.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# sysctl -w net.netfilter.nf_conntrack_max=2621440
+# echo "net.netfilter.nf_conntrack_max=2621440" >> /etc/sysctl.conf
 echo 2621440 | sudo tee /proc/sys/net/netfilter/nf_conntrack_max
